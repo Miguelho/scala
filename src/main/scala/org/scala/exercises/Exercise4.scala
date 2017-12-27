72d3479c6ab5643419ad67b5ff6fd5f8ac86b711
Miguel Halys OrtuÃ±o <mhalys91@gmail.com>
2017-12-27 20:05:06 +0100

    first functional approach

--- src/main/scala/org/scala/exercises/Exercise4.scala
+++ src/main/scala/org/scala/exercises/Exercise4.scala
@@ -1,23 +1,27 @@
 package org.scala.exercises
 
-import org.scala.exercises.exercise4.LinkedList2
+import org.scala.exercises.exercise4.LinkedList
 
 object Exercise4 {
   
   def main( args : Array[String] ) : Unit = {
-    val linkedList = new LinkedList2[String]  
+    val emptyList = LinkedList()
     
-    linkedList.add("Hola")
-    println("Head: " + linkedList.head)
-    linkedList.add("Que tal")
-    println("Tail: " + linkedList.tail)
-    linkedList.add("Adios")
+    println("Is empty? " + emptyList.isEmpty() + "\n")
     
-    print("Value at node 1", linkedList.get(1))
+    val lista = LinkedList(1,2,3,4,5)
     
+    println(lista.size)
+    println("Is empty? " + lista.isEmpty() + "\n")
     
     
-//    linkedList.list
+    val lista$1 = lista.add(6)
+    
+    println(lista$1.size)
+    
+    println(lista$1.get(3))
+    println("Head "+lista$1.head())
+    println("Last "+lista$1.last())
   }
 }
 
--- src/main/scala/org/scala/exercises/exercise4/LinkedList.scala
+++ src/main/scala/org/scala/exercises/exercise4/LinkedList.scala
@@ -1,10 +1,77 @@
 package org.scala.exercises.exercise4
 
-
-class LinkedList[T] {
- 
-  case class Node[T](value: Option[T], tail: LinkedList[T]) extends LinkedList
+/**
+ * LinkedList objects are built from right to left (first we have a tail and then we have the objects
+ * */
+class LinkedList[+T] extends ILinkedList[T] {
   
-  case object EmptyNode extends LinkedList[Nothing]
+  def head(): Option[T] = {get(size) }
+  def last(): Option[T] = { get(1) }
+  def isEmpty(): Boolean = { size == 0 }
+  def get(index: Int): Option[T] = {
+    this match {
+      case Node(elem: T, tail: LinkedList[T]) => {
+        if(size==index) {
+            Option(elem)
+          }else{
+            println("next recursive invocation, exiting " + this)
+            tail.get(index)
+        }
+      }
+      
+      case _ => { 
+        println("Node not found") 
+        Option.empty[T]
+      }
+    }
+    
+    
+    
+  }
+  def add[T](element: T): LinkedList[T]={
+    Node(element, this)
+  }
+  
+  def size(): Int={this.size()}
+  
+  
+}
+/**
+ * el tamaño es una variable inmutable asociada con cada instancia de Node.
+ * 
+ * 1 + el valor de la LinkedList antes de añadir el element
+ * 
+ * */
+case class Node[T](element: T, tail: LinkedList[T]) extends LinkedList{
+  override val size = 1 + tail.size()
+}
+  
+  /**
+   * Valor final de la lista.
+   * */
+  case object EmptyNode extends LinkedList[Nothing]{
+    override val size = 0
+  }
+  
+/**
+   * Companion object
+   * */
+  object LinkedList{
+    def apply[T](items: T*): LinkedList[T] = {
+      if(items.isEmpty)
+        EmptyNode
+      else
+        Node(items.head, apply(items.tail: _*))
+    }
+  }
+
+trait ILinkedList[+T] {
+  
+  def head(): Option[T]
+  def last(): Option[T]
+  def isEmpty(): Boolean
+  def get(index: Int): Option[T]
+  def add[T](element: T): LinkedList[T]
+  def size(): Int
   
 }
